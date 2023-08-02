68c21783f7be346331eaa686ad5d4a01e7dd59bb
khemsu <110565726+khemsu@users.noreply.github.com>
2023-08-02 17:11:12 +0545

    Update Trapezoidal.c

--- Trapezoidal.c
+++ Trapezoidal.c
@@ -5,27 +5,13 @@
 
 int main()
 {
- float lower, upper, integration=0.0, stepSize, k;
- int i, subInterval;
+ float lower, upper, integration=0.0,h;
  printf("Enter lower limit of integration: ");
  scanf("%f", &lower);
  printf("Enter upper limit of integration: ");
  scanf("%f", &upper);
- printf("Enter number of sub intervals: ");
- scanf("%d", &subInterval);
-
- 
- /* Finding step size */
- stepSize = (upper - lower)/subInterval;
-
- /* Finding Integration Value */
- integration = f(lower) + f(upper);
- for(i=1; i<= subInterval-1; i++)
- {
-  k = lower + i*stepSize;
-  integration = integration + 2 * f(k);
- }
- integration = integration * stepSize/2;
+ h=(upper-lower)/2;
+ integration = h*(f(lower) + f(upper));
  printf("\nRequired value of integration is: %.3f", integration);
  return 0;
 }
